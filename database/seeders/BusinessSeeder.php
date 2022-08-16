cddd050bdd2f26a926ff6712e6113067cd54b692
Rafael Pereira <rafaelhpmg@gmail.com>
2022-08-16 18:06:31 -0300

    :sparkles: Migrations, Factories  and Seeders

---
+++ database/seeders/BusinessSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Business;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BusinessSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Business::factory()->count(2)->create();
+        // DB::table('businesses')->insert([]);
+    }
+}
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,25 +1,20 @@
+@extends('layouts.default')
+
+@section('title', 'User Title')
+
+@section('sidebar')
+    <div>
+        <nav>User Sidebar</nav>
+    </div>
+@endsection
+
 {{route('user.show', $user->id)}}
+
+@section('content')
 
 <h1>USER:</h1>
 {{$user->name}} <br>
 {{$user->email}} <br>
 {{date('d-m-Y')}} <br>
 
-@if($user->id)
-    <p>{{$user->name}}</p>
-@endif
-
-@php
-    $bool = false;
-@endphp
-
-
-Unless
-@unless($bool)
-    <p>Falso</p>
-@endunless
-
-If
-@if(!$bool)
-    <p>Verdadeiro</p>
-@endif
+@endsection
